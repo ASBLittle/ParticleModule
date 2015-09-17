ad4a0edb35a5d55c9caf19fb298c4681613f5e5a
James Percival <j.percival@imperial.ac.uk>
2015-09-17 14:48:36 +0100

    Adding extra force terms.

--- particles.py
+++ particles.py
@@ -1,17 +1,92 @@
 import vtk
 import numpy
+import scipy.linalg as la
 import pylab as p
 import itertools
+import glob
 
+class TemporalCache(object):
+    
+    def __init__(self,base_name):
+        files=glob.glob(base_name+'*.vtu')
 
-reader=vtk.vtkXMLUnstructuredGridReader()
+        self.data=[]
+
+        self.lower=0
+        self.upper=0
+
+        
+
+        print files
+
+        for file in files:
+            rdr=vtk.vtkXMLUnstructuredGridReader()
+            rdr.SetFileName(file)
+            for k in range(rdr.GetNumberOfPointArrays()):
+                rdr.SetPointArrayStatus(rdr.GetPointArrayName(k),0)
+            for k in range(rdr.GetNumberOfCellArrays()):
+                rdr.SetCellArrayStatus(rdr.GetCellArrayName(k),0)
+            rdr.SetPointArrayStatus('Time',1)
+            rdr.Update()
+            ug=rdr.GetOutput()
+            t=ug.GetPointData().GetScalars('Time').GetValue(0)
+            
+            self.data.append([t,file,None,None])
+
+        self.data.sort(cmp=lambda x,y:cmp(x[0],y[0]))
+        
+        self.open(0)
+            
+    def reset(self):
+        self.lower=0
+        self.upper=0
+
+    def range(self,a,b):
+        if self.data[self.lower][0]>a:
+            self.reset()
+        while self.lower<len(self.data)-2 and self.data[self.lower+1][0]<=a:
+            self.close(self.lower)
+            self.lower+=1
+        while self.upper<=len(self.data)-2 and self.data[self.upper][0]<=b:
+            self.upper+=1
+            self.open(self.upper)
+
+        return self.data[self.lower:self.upper+1]
+
+    def open(self,k):
+        
+        rdr=vtk.vtkXMLUnstructuredGridReader()
+
+        print 'loading %s'%self.data[k][1]
+        rdr.SetFileName(self.data[k][1])
+        rdr.Update()
+
+        self.data[k][2]=rdr.GetOutput()
+        cl=vtk.vtkCellLocator()
+        cl.SetDataSet(self.data[k][2])
+        cl.BuildLocator()
+        self.data[k][3]=cl
+
+    def close(self,k):
+        del self.data[k][3]
+        del self.data[k][2]
+        self.data[k].append(None)
+        self.data[k].append(None)
+
+    def __call__(self,t):
+        lower=self.lower
+        upper=self.upper
+        
+        while lower<len(self.data)-2 and self.data[lower+1][0]<=t:
+            lower+=1
+
+        t0=self.data[lower][0]
+        t1=self.data[lower+1][0]
+        if t1==t0: t1=1e300
+
+        return self.data[lower:lower+2], (t-t0)/(t1-t0)
+
 bnd_reader=vtk.vtkXMLUnstructuredGridReader()
-
-file1=vtk.vtkUnstructuredGrid()
-file2=vtk.vtkUnstructuredGrid()
-l1=vtk.vtkCellLocator()
-l2=vtk.vtkCellLocator()
-
 bnd_reader.SetFileName('bnd.vtu')
 bfile=bnd_reader.GetOutput()
 bnd_reader.GetOutput().Update()
@@ -27,34 +102,36 @@ class collisionException(Exception):
     pass
 
 class collisionInfo(object):
-    def __init__(self,x,cell,angle,t):
+    def __init__(self,x,v,cell,angle,t):
         self.x=x
+        self.v=v
         self.cell=cell
         self.angle=angle
         self.t=t
 
 class particle(object):
 
-    def __init__(self,p,v,t=0.0,dt=1.0):
+    def __init__(self,p,v,t=0.0,dt=1.0,tc=None):
         
         self.p=p
         self.v=v
         self.t=t
         self.dt=dt
         self.collisions=[]
+        self.tc=tc
+        self.rho=1.0
+        self.g=numpy.array((0.,0.,0.))
+        self.omega=numpy.array((0.,0.,0.))
 
     def update(self):
         # Simple RK4 integration
 
         collision=False
 
-        try:
-            kp1=self.v
-            kv1=self.force(self.p,
+        kp1=self.v
+        kv1=self.force(self.p,
                        self.v,
                        self.t)
-        except:
-            print self.p
 
         s,c1=self.collide(self.p+0.5*self.dt*kp1)
         kp2=self.v+0.5*self.dt*kv1
@@ -92,24 +169,23 @@ class particle(object):
 
         beta=0.44
 
-        u,collision=self.picker(p,t)
+        u,grad_p=self.picker(p,t)
 
 #        if collision:
 #            raise collisionException
 
         drag=beta*(u-v)
 
-        return drag
+        return grad_p/self.rho+drag+(-2.0*numpy.cross(self.omega,v)+self.g+numpy.cross(self.omega,numpy.cross(self.omega,p)))
 
 
     def picker(self,p,t):
 
         
-
-        l1.BuildLocatorIfNeeded()
-        l2.BuildLocatorIfNeeded()
-
         def fpick(file,locator):
+
+            locator.BuildLocatorIfNeeded()
+
 
             cp=[0.0,0.0,0.0]
             ci=vtk.mutable(0)
@@ -141,16 +217,46 @@ class particle(object):
 
             pids=c.GetPointIds()
             
-            data=file.GetPointData().GetVectors('Velocity')
+            data_u=file.GetPointData().GetVectors('Velocity')
+            data_p=file.GetPointData().GetScalars('Pressure')
 
             out=numpy.zeros(3)
-            
-            for k,a in enumerate(x):
-                out+=a*numpy.array(data.GetTuple(pids.GetId(k)))
 
-            return out,collision
+            sf=numpy.zeros(c.GetNumberOfPoints())
+            c.InterpolateFunctions(x,sf)
+
+            rhs=numpy.zeros(2)
+            rhs[0]=data_p.GetValue(c.GetPointId(1))-data_p.GetValue(c.GetPointId(0))
+            rhs[1]=data_p.GetValue(c.GetPointId(2))-data_p.GetValue(c.GetPointId(0))
+
+            A=numpy.zeros((2,2))
+
+
+            p0=numpy.array(c.GetPoints().GetPoint(0))
+            p1=numpy.array(c.GetPoints().GetPoint(1))
+            p2=numpy.array(c.GetPoints().GetPoint(2))
+            A[0,:]=(p1-p0)[:2]
+            A[1,:]=(p2-p0)[:2]
+
+            A=la.inv(A)
             
-        return fpick(file1,l1)
+
+            for k in range(c.GetNumberOfPoints()):
+                out+=sf[k]*numpy.array(data_u.GetTuple(pids.GetId(k)))
+                
+
+            gp=numpy.zeros(3)
+            gp[:2]=numpy.dot(A,rhs)
+
+
+            return out,gp
+
+        data,alpha=self.tc(t)
+
+        u0,gp0=fpick(data[0][2],data[0][3])
+        u1,gp1=fpick(data[1][2],data[1][3])
+            
+        return (1.0-alpha)*u0+alpha*u1,(1.0-alpha)*gp0+alpha*gp1
 
 
     def collide(self,p,v=None,pa=None,dt=None):
@@ -196,7 +302,7 @@ class particle(object):
 
             theta=numpy.arccos(numpy.dot(n,(p-pa))/numpy.sqrt(numpy.dot(p-pa,p-p0)))
 
-            coldat=collisionInfo(x,ci,theta,self.t+s*dt)
+            coldat=collisionInfo(x,v,ci,theta,self.t+s*dt)
 
             if type(v) != type(None):
                 v-=(1.0+e)*n*(numpy.dot(n,v))
@@ -212,30 +318,18 @@ class particle(object):
         else:
             return p-pa, None
                                
-        
-
-
-
-
-reader.SetFileName('simple_0.vtu')
-file1=reader.GetOutput()
-file1.Update()
-
-l1.SetDataSet(file1)
-l2.SetDataSet(file2)
-l1.BuildLocator()
-l2.BuildLocator()
-
 
 class particle_bucket(object):
 
-    def __init__(self,X,V,t,dt,filename='data.dat'):
+    def __init__(self,X,V,t,dt,filename='data.dat',base_name=''):
 
+        self.tc=TemporalCache(base_name)
         self.particles=[]
+
 
         for x,v in zip(X,V):
             print x,v
-            self.particles.append(particle(x,v,t,dt))
+            self.particles.append(particle(x,v,t,dt,tc=self.tc))
         self.t=t
         self.p=X
         self.v=V
@@ -243,6 +337,7 @@ class particle_bucket(object):
         self.file=open(filename,'w')
 
     def update(self):
+        self.tc.range(self.t,self.t+self.dt)
         for p in self.particles:
             p.update()
 
@@ -285,11 +380,11 @@ X=2.0*(numpy.random.random((N,3))-0.5)
 X[:,2]=0
 V=numpy.zeros((N,3))
 
-pb=particle_bucket(X,V,0.0,0.01)
+pb=particle_bucket(X,V,0.0,0.01,base_name='simple')
 pb.write()
 
 
-for k in range(300):
+for k in range(500):
     print pb.t
     pb.update()
     pb.write()
@@ -298,7 +393,7 @@ pb.file.flush()
 
 
 
-def plot(filename='data.dat'):
+def get_data(filename='data.dat'):
     f=open(filename,'r')
 
     t=[]
@@ -329,6 +424,43 @@ def plot(filename='data.dat'):
     x=numpy.array(x)
     y=numpy.array(y)
 
+    f.close()
+
+    return t,x,y
+
+def plot(filename='data.dat'):
+
+    t,x,y = get_data(filename)
+
     p.plot(x,y)
 
+def movie(parbuck,filename='data.dat'):
+
+    col=[c for c in pb.collisions()]
+
+    col.sort(key=lambda x:x.t)
+
+    t,x,y = get_data(filename)
+
+    for k in range(len(t)):
+        p.clf()
+        p.plot(x[:k+1],y[:k+1],zorder=1)
+        p.scatter(x[k],y[k],s=10,zorder=5)
+
+        for c in col:
+            if c.t<t[k]-0.01:
+                p.scatter(c.x[0],c.x[1],c='r',s=30)
+            elif c.t<=t[k]:
+                p.scatter(c.x[0],c.x[1],c='k',s=50)
+            else:
+                break
+
+        p.title('t=%4.3f'%t[k])
+
+        p.savefig('movie/test%05d.png'%k)
         
+p.figure()
+
+movie(pb)
+
+

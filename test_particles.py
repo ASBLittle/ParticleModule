64c1cb502f08f2990eda9077f1f0a640e575bc3d
James Percival <j.percival@imperial.ac.uk>
2015-09-23 18:15:27 +0100

    Test whether particle initializes successfully.

--- test_particles.py
+++ test_particles.py
@@ -1,7 +1,13 @@
 import Particles
 
-
-
 def test_tests():
     assert 1
 
+
+def test_basic_particle_initialization():
+    from numpy import zeros
+
+    p=zeros(3)
+    v=zeros(3)
+
+    p=Particles.particle(p,v)

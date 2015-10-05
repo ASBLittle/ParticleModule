291a6e74ab9ce882044929040a092da5998e53f3
James Percival <j.percival@imperial.ac.uk>
2015-10-05 17:11:53 +0100

    Add regression test for Mclaury.

--- tests/test_particles.py
+++ tests/test_particles.py
@@ -1,22 +1,26 @@
 import Particles
 import IO
+import Collision
 
 import vtk
 import numpy
 
 bnd=IO.boundaryData('tests/data/rightward_boundary.vtu')
 
-def tc(x):
-    r1=vtk.vtkXMLUnstructuredGridReader()
-    r1.SetFileName('tests/data/rightward_0.vtu')
-    r1.Update()
+def tc(fname='rightward_0.vtu'):
+    def fn(x):
+        r1=vtk.vtkXMLUnstructuredGridReader()
+        r1.SetFileName('tests/data/'+fname)
+        r1.Update()
 
-    l1=vtk.vtkCellLocator()
-    l1.SetDataSet(r1.GetOutput())
-    l1.BuildLocator()
+        l1=vtk.vtkCellLocator()
+        l1.SetDataSet(r1.GetOutput())
+        l1.BuildLocator()
 
-    return [[None,None,r1.GetOutput(),l1],
-            [None,None,r1.GetOutput(),l1]], 0.0
+        return [[None,None,r1.GetOutput(),l1],
+                [None,None,r1.GetOutput(),l1]], 0.0
+
+    return fn
 
 
 def test_tests():
@@ -72,7 +76,7 @@ def test_particle_bucket_step_do_nothing(tmpdir):
 
 def test_picker():
 
-    P=Particles.particle(0,0,tc=tc)
+    P=Particles.particle(0,0,tc=tc())
 
     u,gp=P.picker((0.5,0.5,0.0),0.0)
 
@@ -87,7 +91,7 @@ def test_step_constant_velocity():
 
     v0=numpy.array((1.0,0.0,0.0))
 
-    P=Particles.particle(p0,v0,dt=0.1,tc=tc,boundary=bnd)
+    P=Particles.particle(p0,v0,dt=0.1,tc=tc(),boundary=bnd)
     P.update()
     assert all(P.p==numpy.array((0.6,0.5,0)))
     assert P.t==0.1
@@ -100,7 +104,7 @@ def test_step_spin_up():
     p0=numpy.array((0.1,0.5,0.0))
     v0=numpy.array((0.0,0.0,0.0))
 
-    P=Particles.particle(p0,v0,dt=0.001,tc=tc,boundary=bnd)
+    P=Particles.particle(p0,v0,dt=0.001,tc=tc(),boundary=bnd)
     P.update()
     assert all(abs(P.p-numpy.array((0.100345,0.5,0)))<1.e-8)
     assert P.t==0.001
@@ -111,7 +115,7 @@ def test_step_head_on_collision():
     p0=numpy.array((0.9995,0.5,0.0))
     v0=numpy.array((1.0,0.0,0.0))
 
-    P=Particles.particle(p0,v0,dt=0.001,d=numpy.infty,tc=tc,boundary=bnd,e=1.0)
+    P=Particles.particle(p0,v0,dt=0.001,d=numpy.infty,tc=tc(),boundary=bnd,e=1.0)
     P.update()
     assert all(abs(P.p-numpy.array((0.9995,0.5,0)))<1.0e-8)
     assert all(P.v==numpy.array((-1.,0,0)))
@@ -128,7 +132,7 @@ def test_diagonal_collision():
     p0=numpy.array((0.9995,0.4995,0.0))
     v0=numpy.array((1.0,1.0,0.0))
 
-    P=Particles.particle(p0,v0,dt=0.001,d=numpy.infty,tc=tc,boundary=bnd,e=1.0)
+    P=Particles.particle(p0,v0,dt=0.001,d=numpy.infty,tc=tc(),boundary=bnd,e=1.0)
     P.update()
     assert all(abs(P.p-numpy.array((0.9995,0.5005,0)))<1.0e-8)
     assert all(P.v==numpy.array((-1.,1.0,0)))
@@ -139,7 +143,33 @@ def test_diagonal_collision():
     assert P.collisions[0].t-0.0005<1e-8
     assert all(P.collisions[0].v==numpy.array((1.,1.,0.)))
     assert P.collisions[0].angle-numpy.pi/4.0<1e-10
+
+
+def test_gyre_collision():
+
+    ### Regression test for Mclaury coefficient
+
+    bndg=IO.boundaryData('tests/data/gyre_boundary.vtu')
+
+    from math import pi
+
+    p0=numpy.array((0.8,0.45,0.0))
+    v0=numpy.array((2.0*pi,0.0,0.0))
+
+    P=Particles.particle(p0,v0,dt=0.001,d=100.0e-4,tc=tc('gyre_0.vtu'),boundary=bndg,e=1.0)
+
+    for i in range(100):
+        P.update()
+
+    assert P.p[0]<1.0
+    assert P.p[1]<1.0
+    assert P.p[0]>0.0
+    assert P.p[1]>0.0
     
+
+    assert len(P.collisions)==1
+    assert P.collisions[0].x[0]==1.0
+    assert Collision.MclauryMassCoeff(P.collisions[0])-0.17749677523046933<1.0e-8
 
 
 def test_coefficient_of_restitution():
@@ -149,7 +179,7 @@ def test_coefficient_of_restitution():
 
     print bnd.bnd
 
-    P=Particles.particle(p0,v0,dt=0.1,d=numpy.infty,tc=tc,boundary=bnd,e=0.5)
+    P=Particles.particle(p0,v0,dt=0.1,d=numpy.infty,tc=tc(),boundary=bnd,e=0.5)
     P.update()
     assert all(abs(P.p-numpy.array((0.975,0.5,0)))<1.0e-8)
     assert all(P.v==numpy.array((-0.5,0,0)))

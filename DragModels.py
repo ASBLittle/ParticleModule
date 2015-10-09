d2924724961d1155b9e6408ec30caf8a178abb58
James Percival <j.percival@imperial.ac.uk>
2015-10-09 14:58:01 +0100

    Further code cleaning of drag models for pylint.

--- DragModels.py
+++ DragModels.py
@@ -4,15 +4,17 @@ import numpy
 
 def stokes_drag(u, v, d, mu, **kwargs):
     """ Return Stokes drag force for particle parameters"""
+    del kwargs ## supress argument unused warning
     return 9./(2.*d)*mu*(u-v)
 
 def turbulent_drag(u, v, d, **kwargs):
     """ Return turbulent drag force for particle parameters"""
+    del kwargs ## supress argument unused warning
     U=numpy.array(u)
     V=numpy.array(v)
     return 0.44*3.0/32.0/d*numpy.sqrt(sum((U-V)**2))*(U-V)
 
-def transitional_drag(u, v, diameter, rho_f=1.0e3, mu=1.0e-3, **kwargs):
+def transitional_drag(fluid_velocity,particle_velocity, diameter, rho_f=1.0e3, mu=1.0e-3, **kwargs):
     """ Return transitional drag force for particle parameters
 
     Transition to turbulence is assumed to occur when Re>1000 for a particle Reynolds number, 
@@ -20,8 +22,9 @@ def transitional_drag(u, v, diameter, rho_f=1.0e3, mu=1.0e-3, **kwargs):
     Re=rho_f*du*diameter/ mu
     
     """
-    U=numpy.array(u)
-    V=numpy.array(v)
+    del kwargs ## supress argument unused warning
+    U=numpy.array(fluid_velocity)
+    V=numpy.array(particle_velocity)
 
     du=numpy.sqrt(sum((U-V)**2))
 

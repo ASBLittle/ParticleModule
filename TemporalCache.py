014085cf40ad0cf9704309f2a396aa9d34782540
James Percival <j.percival@imperial.ac.uk>
2015-10-12 11:25:18 +0100

    Lint cleaning TemporalCache.py

--- TemporalCache.py
+++ TemporalCache.py
@@ -5,15 +5,16 @@ import glob
 import numpy
 
 class TemporalCache(object):
-    """ The base object containing the vtu files. 
+    """ The base object containing the vtu files.
 
-    This scans the files for their timelevel information and provides a pair of files bracketing the desired timelevel when called
+    This scans the files for their timelevel information and provides a pair
+    of files bracketing the desired timelevel when called
     """
-    def __init__(self,base_name,t_min=0.,t_max=numpy.infty):
-        """ 
+    def __init__(self, base_name, t_min=0., t_max=numpy.infty):
+        """
         Initialise the cache from a base file name and optional limits on the time levels desired.
         """
-        files=glob.glob(base_name+'*.vtu')
+        files = glob.glob(base_name+'_[0-9]*.vtu')
 
         self.data = []
         self.reset()
@@ -29,34 +30,38 @@ class TemporalCache(object):
                 rdr.SetCellArrayStatus(rdr.GetCellArrayName(k), 0)
             rdr.SetPointArrayStatus('Time', 1)
             rdr.Update()
-            ug = rdr.GetOutput()
-            t = ug.GetPointData().GetScalars('Time').GetValue(0)
-            
-            self.data.append([t, filename,None,None])
+            ugrid = rdr.GetOutput()
+            time = ugrid.GetPointData().GetScalars('Time').GetValue(0)
 
-        self.data.sort(cmp=lambda x, y:cmp(x[0], y[0]))
+            self.data.append([time, filename, None, None])
+
+        self.data.sort(cmp=lambda x, y: cmp(x[0], y[0]))
 
         self.range(t_min, t_max)
-            
+
     def reset(self):
         """ Reset the bounds on the loaded cache data"""
-        for k,d in enumerate(self.data):
-            if d[2]: self.close(k)
+        for k, dat in enumerate(self.data):
+            if dat[2]:
+                self.close(k)
         self.lower = 0
         self.upper = 0
 
-    def range(self,a,b):
+    def range(self, t_min, t_max):
         """ Specify a range of data to keep open."""
-        if len(self.data) == 0: return
-        if self.data[self.lower][0] > a:
+        if len(self.data) == 0:
+            return
+        if self.data[self.lower][0] > t_min:
             self.reset()
-        while self.lower < len(self.data)-2 and self.data[self.lower+1][0] <= a:
+        while (self.lower < len(self.data)-2
+               and self.data[self.lower+1][0] <= t_min):
             self.close(self.lower)
             self.lower += 1
-        if self.upper <= self.lower: 
+        if self.upper <= self.lower:
             self.upper = self.lower
             self.open(self. lower)
-        while self.upper <= len(self.data)-2 and self.data[self.upper][0] <= b:
+        while (self.upper <= len(self.data)-2
+               and self.data[self.upper][0] <= t_max):
             self.upper += 1
             self.open(self.upper)
 
@@ -64,17 +69,17 @@ class TemporalCache(object):
 
     def open(self, k):
         """ Open a file for reading."""
-        rdr=vtk.vtkXMLUnstructuredGridReader()
+        rdr = vtk.vtkXMLUnstructuredGridReader()
 
         print 'loading %s'%self.data[k][1]
         rdr.SetFileName(self.data[k][1])
         rdr.Update()
 
         self.data[k][2] = rdr.GetOutput()
-        cl=vtk.vtkCellLocator()
-        cl.SetDataSet(self.data[k][2])
-        cl.BuildLocator()
-        self.data[k][3] = cl
+        cloc = vtk.vtkCellLocator()
+        cloc.SetDataSet(self.data[k][2])
+        cloc.BuildLocator()
+        self.data[k][3] = cloc
 
     def close(self, k):
         """Close an open file (implictly through the garbage collector."""
@@ -83,18 +88,19 @@ class TemporalCache(object):
         self.data[k].append(None)
         self.data[k].append(None)
 
-    def __call__(self, t):
-        """ Get the data bracketing time level t."""
+    def __call__(self, time):
+        """ Get the data bracketing time level."""
         lower = self.lower
         upper = self.upper
 
-        assert self.data[lower][0] <= t and self.data[upper][0] >= t
-        
-        while lower < len(self.data)-2 and self.data[lower+1][0] <= t:
-            lower+=1
+        assert self.data[lower][0] <= time and self.data[upper][0] >= time
 
-        t0 = self.data[lower][0]
-        t1 = self.data[lower+1][0]
-        if t1 == t0: t1 = numpy.infty
+        while lower < len(self.data)-2 and self.data[lower+1][0] <= time:
+            lower += 1
 
-        return self.data[lower:lower+2], (t-t0)/(t1-t0)
+        t_min = self.data[lower][0]
+        t_max = self.data[lower+1][0]
+        if t_max == t_min:
+            t_max = numpy.infty
+
+        return self.data[lower:lower+2], (time-t_min)/(t_max-t_min)

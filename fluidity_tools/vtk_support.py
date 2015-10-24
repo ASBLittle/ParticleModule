d619d5b45b5eff27bfe3a6154f80389a115bc4dd
James Percival <j.percival@imperial.ac.uk>
2015-10-24 23:46:48 +0100

    Outputting the fluidity data to vtk.

--- fluidity_tools/vtk_support.py
+++ fluidity_tools/vtk_support.py
@@ -30,7 +30,47 @@ def fluidity_to_ugrid(state):
 
         ugrid.InsertNextCell(CELL_DICT[(shape.type, shape.dimension, shape.loc)],id_list)
 
+    for name, scalar in state.scalar_fields.items():
+
+        data = vtk.vtkDoubleArray()
+        data.SetName(name)
+        data.Allocate(ugrid.GetNumberOfPoints())
+
+        if scalar.node_count == 1:
+            for k in range(ugrid.GetNumberOfPoints()):
+                data.InsertNextValue(scalar[0])
+        else:
+            for k in range(ugrid.GetNumberOfPoints()):
+                data.InsertNextValue(scalar[k])
+
+        print name, ugrid.GetNumberOfPoints(), data.GetNumberOfTuples()
+        ugrid.GetPointData().AddArray(data)
+
+    for name, vector in state.vector_fields.items():
+        
+        data = vtk.vtkDoubleArray()
+        data.SetName(name)
+        data.SetNumberOfComponents(3)
+
+        if vector.node_count == 1:
+            for k in range(ugrid.GetNumberOfPoints()):
+                lval=numpy.zeros(3)
+                lval[:vector.val.shape[1]] = vector.node_val(0)
+                data.InsertNextTuple3(*(lval))
+        else:
+            for k in range(ugrid.GetNumberOfPoints()):
+                lval=numpy.zeros(3)
+                lval[:vector.val.shape[1]] = vector.node_val(k)
+                data.InsertNextTuple3(*(lval))
+
+        print name, ugrid.GetNumberOfPoints(), data.GetNumberOfTuples()
+        ugrid.GetPointData().AddArray(data)
+
+
+
     return ugrid
+
+
         
     
 

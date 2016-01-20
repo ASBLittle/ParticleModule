3ff07226d26688fd2e4b474280b400937ec1f008
James Percival <j.percival@imperial.ac.uk>
2016-01-20 21:21:47 +0000

    Moving the boundary with the flow.

---
+++ examples/move_boundaries/drive_from_fluidity.py
@@ -0,0 +1,31 @@
+"""Example of particles from an options file."""
+import particle_model as pm
+import numpy
+import vtk
+
+
+def setup(mb,time,dt):
+    N = 0
+
+    X = 0.5+0.5*(numpy.random.random((N, 3))-0.5)
+    X[:,0] += 0.2
+    X[:,2] = 0
+
+    V = numpy.zeros((N,3))
+    V[:,0] = 1.0
+
+
+    SYSTEM=pm.System.get_system_from_options(block=(mb,time,dt))
+    PAR=pm.ParticleBase.get_parameters_from_options()[0]
+
+    writer=vtk.vtkXMLUnstructuredGridWriter()
+    writer.SetFileName('boundary.vtu')
+    writer.SetInput(SYSTEM.boundary.bnd)
+    writer.Write()
+
+    PB = pm.Particles.ParticleBucket(X, V, time, 1.0e-2,
+                                 system=SYSTEM,
+                                 parameters=PAR)
+
+
+    return PB

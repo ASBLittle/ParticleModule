c0b3716583a5be28bae025a29915bd2b96e7ff3e
James Percival <j.percival@imperial.ac.uk>
2019-06-20 15:37:04 +0100

    Remove accidental print command.

--- particle_model/Coupling.py
+++ particle_model/Coupling.py
@@ -149,7 +149,6 @@ def get_wear_rate_source(bucket, alpha, delta_t):
         for dummy_1 in range(pnt_ids.GetNumberOfIds()):
             volume[pnt_ids.GetId(dummy_1)] += cv_mass
 
-    print(len(bucket.collisions()))
     for col in bucket.collisions():
         if col.time < bucket.time-bucket.delta_t:
             continue
